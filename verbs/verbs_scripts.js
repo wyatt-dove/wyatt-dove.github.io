e256b7c4d3f961d690b2b48c5949dda57ef0b852
Wyatt Dove <143294093+wyatt-dove@users.noreply.github.com>
2024-11-02 14:59:49 -0500

    Initial commit
    All project files

---
+++ verbs/verbs_scripts.js
@@ -0,0 +1,23 @@
+//Written by Ashia Taplin, modified slightly by Wyatt Dove
+function openTab(event, tabName) {
+    var i, tabcontent, tablinks;
+  
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    // Remove "active" class from all tab links
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab and add "active" class to the clicked tab link
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
+  // Set the first tab to be visible by default
+  document.getElementById("tab-link active").style.display = "block";  
